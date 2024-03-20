ce655267871ab8e0119995f86228003a2cddb340
Dylan Guzman <70229022+dgoose2@users.noreply.github.com>
2024-03-20 14:51:17 -0700

    Minor Changes for Premier Ikon

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,8 +3,9 @@ import Navbar from "@/components/navigation-navbar/Navbar";
 import Footer from "@/components/navigation-footer/Footer";
 
 export const metadata = {
-  title: "necti",
-  description: "Website Development",
+  title: "Premier Ikon",
+  description:
+    "Elevating Brands to Iconic Status. Specializing in Global Merchandising, Innovative Product Development, and Dynamic Branding Strategies, we empower brands to captivate and connect with audiences worldwide. Our passion lies in transforming inspiration into reality, championing creativity, and forging genuine, authentic connections.",
 };
 
 export default function RootLayout({ children }) {
